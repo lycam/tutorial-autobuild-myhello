ec3ab8164ce50307eb53429bfe228c0128786c09
no777 <no777.nobody@gmail.com>
2015-12-13 16:34:31 +0800

    no message

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var http = require("http");
 var os = require("os");
-var port = 80;
+var port = 8080;
 http.createServer(function(request, response) {
     console.log("New request arrived.");
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hi, you are visiting " + os.hostname() + "\n");
     response.end();
 }).listen(port);
+console.log("this is a test");
 console.log("Server is running, listening on port "+port+"....");
