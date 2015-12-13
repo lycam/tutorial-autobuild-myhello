866ba63ddc7a12490fc0db71dda920bc032b0d3a
no777 <no777.nobody@gmail.com>
2015-12-13 16:40:45 +0800

    no message

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var port = 80;
 http.createServer(function(request, response) {
     console.log("New request arrived.");
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hi, you are visiting " + os.hostname() + "\n");
+    response.write("Hi, you are visiting " + os.hostname() + ",this is a test for docker\n");
     response.end();
 }).listen(port);
-console.log("this is a test");
+console.log("this is a test for docker");
 console.log("Server is running, listening on port "+port+"....");
